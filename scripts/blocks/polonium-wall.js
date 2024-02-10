7ee563dd201317e2da9a234ecce9b86834014e68
7rodo <7rodovje7878@gmail.com>
2024-02-10 22:02:26 +0000

    fk,der

--- scripts/blocks/polonium-wall.js
+++ scripts/blocks/polonium-wall.js
@@ -1,4 +1,4 @@
-update(tile){
+update(tile);{
   var r = 9 * Vars.tilesize;
   Units.nearbyEnemies(tile.getTeam(), tile.drawx() - r, tile.drawy() - r, 2 * r, 2 * r, cons(unit => {
     if(unit instanceof HealthTrait && !unit.isDead() && unit.withinDist(tile.drawx(), tile.drawy(), r)){
@@ -8,4 +8,4 @@ update(tile){
     }
   }));
 }
-}
+
