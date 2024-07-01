ae95af434eecf0be8e19cacad1c2d84ba0c0ad00
coal deficit <159595900+coaldeficit@users.noreply.github.com>
2024-07-01 17:27:20 +0200

    Update and rename artilleryman.js to pounder.js

--- scripts/units/ground/shotgunner/artilleryman.js
+++
@@ -1,8 +0,0 @@
-const ShotT2 = extend(UnitType, "artilleryman-mech", {});
-ShotT2.constructor = () => extend(MechUnit, {});
-ShotT2.immunities.add(StatusEffects.corroded);
-
-Blocks.additiveReconstructor.addUpgrade(
-  Vars.content.getByName(ContentType.unit, "md3-shotgunner-mech"),
-  Vars.content.getByName(ContentType.unit, "md3-artilleryman-mech")
-)
