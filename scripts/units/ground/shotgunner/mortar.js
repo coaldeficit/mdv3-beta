8da84fa122bac4a9ab8d71ee38b26e99b815e51d
7rodo <7rodovje7878@gmail.com>
2024-07-14 17:07:39 +0100

    shotT3 added

--- scripts/units/ground/shotgunner/mortar.js
+++ scripts/units/ground/shotgunner/mortar.js
@@ -3,6 +3,6 @@ ShotT4.constructor = () => extend(LegsUnit, {});
 ShotT4.immunities.add(StatusEffects.corroded);
 
 Blocks.exponentialReconstructor.addUpgrade(
-  Vars.content.getByName(ContentType.unit, "md3-ordnance-mech"),
+  Vars.content.getByName(ContentType.unit, "md3-slugger-mech"),
   Vars.content.getByName(ContentType.unit, "md3-mortar-mech")
 )
--- scripts/units/ground/shotgunner/slugger.js
+++ scripts/units/ground/shotgunner/slugger.js
@@ -1,8 +1,8 @@
-const ShotT3 = extend(UnitType, "ordnance-mech", {});
+const ShotT3 = extend(UnitType, "slugger-mech", {});
 ShotT3.constructor = () => extend(LegsUnit, {});
 ShotT3.immunities.add(StatusEffects.corroded);
 
 Blocks.multiplicativeReconstructor.addUpgrade(
   Vars.content.getByName(ContentType.unit, "md3-pounder-mech"),
-  Vars.content.getByName(ContentType.unit, "md3-ordnance-mech")
+  Vars.content.getByName(ContentType.unit, "md3-slugger-mech")
 )
