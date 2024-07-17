4dab18732dd998ab1d20d9b5222b41c3a553d9fa
coal deficit <159595900+coaldeficit@users.noreply.github.com>
2024-07-17 21:26:12 +0200

    bruh

--- scripts/blocks/environment.js
+++ scripts/blocks/environment.js
@@ -15,7 +15,7 @@ let hotNiebieskite = extend(Floor, "niebieskite-hot", {});
 hotNiebieskite.attributes.set(Attribute.heat, 0.5);
 let hotMetacinnabar = extend(Floor, "metacinnabar-hot", {});
 hotMetacinnabar.attributes.set(Attribute.heat, 0.5);
-let dimethyl = extend(Floor, "dimethylmercury", {});
+let dimethyl = extend(Floor, "dimethyl", {});
 
 module.exports = {
   //vanadinite: vanadinite,
