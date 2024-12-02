409667f7606905d6d8c590f4162bec82f194c055
CD <159595900+coaldeficit@users.noreply.github.com>
2024-12-02 02:36:06 +0100

    rocketeer resprite

--- scripts/units/ground/shotgunner/rocketeer.js
+++ scripts/units/ground/shotgunner/rocketeer.js
@@ -8,8 +8,8 @@ const missile = extend(MissileUnitType, "rocketeer-mech-missile", {
   maxRange: 6,
   lifetime: 60 * 1.4,
   outlineColor: Pal.darkOutline,
-  engineColor: Pal.sapBulletBack,
-  trailColor: Pal.sapBulletBack,
+  engineColor: Color.valueOf("#FFA665"),
+  trailColor: Color.valueOf("#FFA665"),
   engineLayer: Layer.effect,
   health: 190,
   loopSoundVolume: 0.1
@@ -32,13 +32,15 @@ missile.weapons.add(missileExplosion)
 
 const launcher = extend(Weapon, {
   name: "md3-rocketeer-missile-launcher",
-  y: 3,
-  x: 20,
+  y: -3,
+  x: 13,
   top: true,
   inaccuracy: 0,
   reload: 90,
   shootSound: Sounds.missileLarge,
   recoil: 6,
+  baseRotation: -7,
+  shootCone: 60,
   bullet: bulLib.makeBullet({
     type: BulletType,
     shootEffect: Fx.shootBig,
