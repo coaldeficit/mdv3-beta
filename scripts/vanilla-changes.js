d32abd98340d6eab1eb51548bf37a54afb119f92
CD <159595900+coaldeficit@users.noreply.github.com>
2024-12-16 20:50:40 +0100

    i may have gone overboard
    eh whatever

--- scripts/vanilla-changes.js
+++ scripts/vanilla-changes.js
@@ -26,3 +26,21 @@ UnitTypes.poly.itemCapacity = 20
 UnitTypes.mega.itemCapacity = 40
 UnitTypes.quad.itemCapacity = 80
 UnitTypes.oct.itemCapacity = 140
+
+// ON CLIENT LOAD
+Events.on(ClientLoadEvent, e => {
+  // NUMBERED ENEMY BASES
+  const convertToBase = [
+    85,223, // its strictly personal
+    92,93,94,96,155, // plt area
+    53,54,79,80,120,253,263,264, // south pole
+    72,132,266,270,271, // hard drive area
+    26,66,67,128,235, // north pole
+    2,9,17,41,78,197 // misc sectors
+  ]
+  for (let i=0;i<convertToBase.length;i++) {
+    Planets.serpulo.sectors.get(convertToBase[i]).generateEnemyBase = true
+  }
+  Planets.serpulo.sectors.get(182).generateEnemyBase = false // enemy base moved to sector 17
+  Planets.serpulo.updateBaseCoverage()
+})
