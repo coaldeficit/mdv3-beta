70c8a9f860d4cb3fce8c671657d4141fd6807f3d
coal deficit <159595900+coaldeficit@users.noreply.github.com>
2024-12-18 11:39:11 +0100

    Update rng.js

--- scripts/libs/rng.js
+++ scripts/libs/rng.js
@@ -1,11 +1,11 @@
 let rngindex = 0
 let rngtable = []
 for (let i=0;i<10000;i++) {
-  rngtable.push(Math.round(Math.pow(i+10000,1.6457343+((i+400)/4000)))%1000)
+  rngtable.push(Math.floor(Math.pow(i+10000,1.6457343+((i+400)/4000)))%1000)
 }
 
 function setIndex(index) {
-  return rngindex = Math.round(index)%1000
+  return rngindex = Math.round(index)%10000
 }
 function randomUnsynced() { // named so because if you run this without first setting an index that is the same for everyone it'll desync in mp
   let a = rngtable[rngindex]
