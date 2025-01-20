813307e9bd2410ce74d6e663421fab1ce680d01f
CD <159595900+coaldeficit@users.noreply.github.com>
2025-01-20 13:36:02 +0100

    oops i implemented PR 10374 myself

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ require("md3/settings")
 require("md3/autoupdate")
 require("md3/libs/shaders")
 
+require("md3/planets")
 require("md3/items")
 require("md3/units/damagegroups")
 require("md3/status-effects")
