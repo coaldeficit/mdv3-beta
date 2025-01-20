8258e168e1f9ec6279e05a304c6674b41aa50605
CD <159595900+coaldeficit@users.noreply.github.com>
2025-01-21 00:32:44 +0100

    ready for balancing

--- scripts/blocks/sporangia.js
+++ scripts/blocks/sporangia.js
@@ -8,14 +8,6 @@ let sporangia = extend(PowerTurret, "panaeolus-subturret", {
   update: true,
   rebuildable: false,
   destroySound: Sounds.plantBreak,
-  reload: 60,
-  shootType: bulLib.makeBullet({
-    type: BasicBulletType,
-    splashDamage: 110,
-    splashDamageRadius: 25,
-    shootEffect: Fx.massiveExplosion,
-    collidesAir: false,
-  }),
 });
 sporangia.buildType = () => extend(PowerTurret.PowerTurretBuild, sporangia, {
   update(){
