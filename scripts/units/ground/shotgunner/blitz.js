1efbe9d27b2088fae70130cbabfd48da84544211
CD <159595900+coaldeficit@users.noreply.github.com>
2025-03-18 00:49:47 +0100

    blitz part 3

--- scripts/units/ground/shotgunner/blitz.js
+++ scripts/units/ground/shotgunner/blitz.js
@@ -1,12 +1,13 @@
 const subturrets = require("md3/blocks/subturrets")
 const blockcheck = require("md3/libs/blockcheck")
+const bulLib = require("md3/libs/bulletlib")
 
 const ShotT5 = extend(UnitType, "blitz-mech", {});
 ShotT5.constructor = () => extend(LegsUnit, {});
 
 const turretBullet = extend(BasicBulletType, {
   speed: 4,
-  lifetime: 60,
+  lifetime: 40,
   damage: 190,
   shootEffect: Fx.shootBig,
   smokeEffect: Fx.shootBigSmoke2,
@@ -16,17 +17,23 @@ const turretBullet = extend(BasicBulletType, {
   collidesAir: false,
   width: 30,
   height: 45,
-  splashDamage: 800,
+  splashDamage: 400,
   splashDamageRadius: 32,
+  despawnSound: Sounds.dullExplosion,
   despawned(b) {
     this.super$despawned(b)
     let develop = true
+    let valid = 0
     blockcheck.iterateSquare(Math.round(b.x/8)-1,Math.round(b.y/8)-1,3,3,(other => {
       if (other.block() != Blocks.air) {
         develop = false
       }
+      valid++
     }));
-    if (develop) Vars.world.tile(Math.round(b.x/8), Math.round(b.y/8)).setNet(subturrets.blade, b.owner.team, 0)
+    if (develop && valid >= 9) {
+      Vars.world.tile(Math.round(b.x/8), Math.round(b.y/8)).setNet(subturrets.blade, b.owner.team, 0)
+      Sounds.place.at(b.x, b.y, Mathf.random(0.8,1.2))
+    }
   }
 })
 const turretLauncher = extend(Weapon, {
@@ -41,8 +48,46 @@ const turretLauncher = extend(Weapon, {
   shootCone: 2,
   bullet: turretBullet,
 });
+const missileLauncher = extend(Weapon, {
+  name: "zenith-missiles",
+  y: 6,
+  x: 17,
+  top: true,
+  rotate: true,
+  rotateSpeed: 5,
+  inaccuracy: 10,
+  reload: 160,
+  shootSound: Sounds.artillery,
+  shootCone: 100,
+  velocityRnd: 0.2,
+  ejectEffect: Fx.none,
+  bullet: bulLib.makeBullet({
+    type: MissileBulletType,
+    speed: 2.7,
+    damage: 24,
+    width: 8,
+    height: 8,
+    shrinkY: 0,
+    drag: -0.003,
+    homingRange: 60,
+    keepVelocity: false,
+    splashDamageRadius: 35,
+    splashDamage: 43,
+    lifetime: 39,
+    trailColor: Pal.unitBack,
+    backColor: Pal.unitBack,
+    frontColor: Pal.unitFront,
+    hitEffect: Fx.blastExplosion,
+    despawnEffect: Fx.blastExplosion,
+    weaveScale: 6,
+    weaveMag: 1,
+  }),
+});
+missileLauncher.shoot.shots = 16
+missileLauncher.shoot.shotDelay = 2
 ShotT5.weapons.add(
-  turretLauncher
+  turretLauncher,
+  missileLauncher,
 );
 
 Blocks.tetrativeReconstructor.addUpgrade(
